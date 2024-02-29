cc085c2e8f76147e0449a2949998a766f9c018b6
nash <nashbates13@gmail.com>
2024-02-28 21:24:13 -0600

    actual source lol

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Mod2
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.author.gorillatag.mod2";
+        public const string Name = "Mod2";
+        public const string Version = "1.0.0";
+    }
+}
