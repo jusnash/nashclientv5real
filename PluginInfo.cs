af4d4eacd3e0b15f4d1732977f1c9597ef6eb7f3
Sohdah <157318177+sohdahh@users.noreply.github.com>
2024-02-29 15:44:57 -0600

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,8 +5,8 @@
     /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.author.gorillatag.mod2";
-        public const string Name = "Mod2";
+        public const string GUID = "com.jusnash.gorillatag.mod2";
+        public const string Name = "NashNoGtag";
         public const string Version = "1.0.0";
     }
 }
